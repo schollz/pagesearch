bf69a6f0f737b67858a25f19afd7ee3d96f78af9
Zack Scholl <zack.scholl@gmail.com>
2018-10-07 07:14:42 -0700

    get data url

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/schollz/pagesearch
 
 require (
 	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575
-	github.com/pkg/errors v0.8.0
-	github.com/schollz/sqlite3dump v1.2.1
-	github.com/stretchr/testify v1.2.2
+	github.com/gin-contrib/sse v0.0.0-20170109093832-22d885f9ecc7 // indirect
+	github.com/gin-gonic/gin v1.3.0
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.4 // indirect
+	github.com/ugorji/go/codec v0.0.0-20180927125128-99ea80c8b19a // indirect
+	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
+	gopkg.in/yaml.v2 v2.2.1 // indirect
 )
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	log "github.com/cihub/seelog"
+	"github.com/gin-gonic/gin"
+)
+
+type PostSearch struct {
+	SearchTerm string `json:"search"`
+	DataURL    string `json:"url"`
+}
+
+func main() {
+	port := "8185"
+	setLogLevel("debug")
+
+	// setup gin server
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
+	// Standardize logs
+	r.Use(middleWareHandler(), gin.Recovery())
+
+	// Example for binding JSON ({"user": "manu", "password": "123"})
+	r.POST("/search", func(c *gin.Context) {
+		var ps PostSearch
+		if err := c.ShouldBindJSON(&ps); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		// get data URL
+
+		c.JSON(http.StatusOK, gin.H{"status": "you are logged in"})
+	})
+	log.Infof("Running at http://0.0.0.0:" + port)
+	err := r.Run(":" + port)
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+func middleWareHandler() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		t := time.Now()
+		// Add base headers
+		addCORS(c)
+		// Run next function
+		c.Next()
+		// Log request
+		log.Infof("%v %v %v %s", c.Request.RemoteAddr, c.Request.Method, c.Request.URL, time.Since(t))
+	}
+}
+
+func addCORS(c *gin.Context) {
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
+	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+}
+
+// setLogLevel determines the log level
+func setLogLevel(level string) (err error) {
+
+	// https://en.wikipedia.org/wiki/ANSI_escape_code#3/4_bit
+	// https://github.com/cihub/seelog/wiki/Log-levels
+	appConfig := `
+	<seelog minlevel="` + level + `">
+	<outputs formatid="stdout">
+	<filter levels="debug,trace">
+		<console formatid="debug"/>
+	</filter>
+	<filter levels="info">
+		<console formatid="info"/>
+	</filter>
+	<filter levels="critical,error">
+		<console formatid="error"/>
+	</filter>
+	<filter levels="warn">
+		<console formatid="warn"/>
+	</filter>
+	</outputs>
+	<formats>
+		<format id="stdout"   format="%Date %Time [%LEVEL] %File %FuncShort:%Line %Msg %n" />
+		<format id="debug"   format="%Date %Time %EscM(37)[%LEVEL]%EscM(0) %File %FuncShort:%Line %Msg %n" />
+		<format id="info"    format="%Date %Time %EscM(36)[%LEVEL]%EscM(0) %File %FuncShort:%Line %Msg %n" />
+		<format id="warn"    format="%Date %Time %EscM(33)[%LEVEL]%EscM(0) %File %FuncShort:%Line %Msg %n" />
+		<format id="error"   format="%Date %Time %EscM(31)[%LEVEL]%EscM(0) %File %FuncShort:%Line %Msg %n" />
+	</formats>
+	</seelog>
+	`
+	logger, err := log.LoggerFromConfigAsBytes([]byte(appConfig))
+	if err != nil {
+		return
+	}
+	log.ReplaceLogger(logger)
+	return
+}
