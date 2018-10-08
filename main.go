7c8064b50cc7a980cb03d0a0c890208205473b1f
Zack Scholl <zack.scholl@gmail.com>
2018-10-08 10:37:51 -0700

    allow options

--- main.go
+++ main.go
@@ -140,7 +140,7 @@ func middleWareHandler() gin.HandlerFunc {
 func addCORS(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET,POST")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 }
